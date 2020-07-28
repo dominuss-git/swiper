8a4fcec37e5d655440bfd834cdfaff86bc35c285
Dominuss-afk <kiril6701@gmail.com>
2020-07-29 00:14:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,46 +3,87 @@ import "../css/style.css";
 // import "../css/lightbox.min.css";
 
 const photos = ["../images/IH4AgXeHhOk.jpg", "../images/Без названия.png"];
-let current_index = 1;
+let current_index;
+let el_array = [];
 
 $(window).ready(() => {
-  $(".my-gallery__prev").click();
+  // $(".my-gallery__prev").click();
+  init();
+  set_data();
+  setOnClickListener();
 });
 
-const addPic = (link) => {
-  return `
-  <img class="my-gallery__my-photo" src="${link}">
-  `;
+const init = () => {
+  for (let i in photos) {
+    $(".my-gallery-update").append(init_up_gallery(photos[i]));
+    // console.log(photos[i]);
+  }
 };
 
-$(".my-gallery__prev").click(() => {
-  if (current_index === 0) {
-    return;
-  } else {
-    current_index -= 1;
-    $(".my-gallery__my-photo").remove();
-    $(".my-gallery").append(addPic(photos[current_index]));
+const setOnClickListener = () => {
+  for (let i = 0; i < el_array.length; i+=1 ) {
+    el_array[i].$el.click(() => {
+      console.log(el_array[i].photo);
+      $(".modal").addClass("modal-active");
+      $(".modal__body").addClass("modal-active__body");
+      $(".modal__img").append(`<img class="my-gallery-update__photo" src="${el_array[i].photo}">`);
+      current_index = i;
+      // console.log(el_array.length - 1);
+      if(current_index === 0) {
+        $(".modal__prev").addClass("disabled");
+      }
+      if(current_index === el_array.length - 1) {
+        $(".modal__next").addClass("disabled");
+      }
+    });
   }
-  if (current_index === 0) {
-    $(".my-gallery__prev").addClass("disabled");
-    $(".my-gallery__next").removeClass("disabled");
-  } else {
-    $(".my-gallery__prev").removeClass("disabled");
-  }
-});
+};
 
-$(".my-gallery__next").click(() => {
-  if (current_index === photos.length -1) {
-    return;
-  } else {
-    current_index += 1;
-    $(".my-gallery__my-photo").remove();
-    $(".my-gallery").append(addPic(photos[current_index]));
+const set_data = () => {
+  $(".my-gallery-update__my-photo").each((i, el) => {
+    // $(this).css("width", "100px");
+    console.log($(el));
+    el_array.push({
+      $el : $(el),
+      photo : photos[i]
+    })
+  });
+};
+
+$(".modal__close").click(() => {
+  $(".modal").removeClass("modal-active");
+  $(".modal__body").removeClass("modal-active__body");
+  $(".my-gallery-update__photo").remove();
+  $(".modal__prev").removeClass("disabled");
+  $(".modal__next").removeClass("disabled");
+})
+
+$(".modal__prev").click(() => {
+  current_index -= 1;
+  console.log(current_index);
+  if(current_index === 0) {
+    $(".modal__prev").addClass("disabled");
   }
-  if (current_index === photos.length -1) {
-    $(".my-gallery__next").addClass("disabled");
-    $(".my-gallery__prev").removeClass("disabled");
-  } else {
-    $(".my-gallery__next").removeClass("disabled");
+  if(current_index !== el_array.length - 1) {
+    $(".modal__next").removeClass("disabled");
   }
-});
+  $(".my-gallery-update__photo").remove();
+  $(".modal__img").append(`<img class="my-gallery-update__photo" src="${el_array[current_index].photo}">`);
+}) 
+
+$(".modal__next").click(() => {
+  current_index += 1;
+  console.log(current_index);
+  if(current_index === el_array.length - 1) {
+    $(".modal__next").addClass("disabled");
+  }
+  if(current_index !== 0) {
+    $(".modal__prev").removeClass("disabled");
+  }
+  $(".my-gallery-update__photo").remove();
+  $(".modal__img").append(`<img class="my-gallery-update__photo" src="${el_array[current_index].photo}">`);
+}) 
+
+const init_up_gallery = (link) => {
+  return `<img class="my-gallery-update__my-photo" src="${link}">`;
+};
